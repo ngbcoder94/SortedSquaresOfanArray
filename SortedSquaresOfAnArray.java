bc80d320f85410563e95cec6789ef69e8bfee957
ngbcoder94 <ngbcoder94@gmail.com>
2024-12-30 14:55:25 -0500

    SortedSquaresOfAnArray.java created

---
+++ SortedSquaresOfAnArray.java
@@ -0,0 +1,15 @@
+/*
+ * This is my class for the Leetcode problem Squares of sorted arra. 
+ *
+ * Author: Nick Burkett
+ * Date: 12/30/24
+ *
+ * Status - WIP
+ */
+public class SortedSquaresOfAnArray{
+
+        public static void main(String[] args){
+                
+                System.out.println("Hell yea");
+        }
+}
